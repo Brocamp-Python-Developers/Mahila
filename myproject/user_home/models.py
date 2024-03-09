92869ca340fa5e7b5ed4ec529174d8fb3d3bdd01
Anurag K <anuragremeshank@gmail.com>
2024-03-09 19:26:34 +0530

    added support helpline

--- myproject/user_home/models.py
+++ myproject/user_home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Messages(models.Model):
+    name = models.CharField(max_length=255)
+    email=models.EmailField()
+    phone_no=models.CharField(max_length=10)
+    message=models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
--- myproject/user_home/views.py
+++ myproject/user_home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import*
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,4 +11,13 @@ def awareness_and_education(request):
     return render(request, 'user_home/awareness_and_education.html')
 
 def support_helpline_section(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone_no = request.POST.get('phone')
+        message_text = request.POST.get('message')
+
+        Messages.objects.create(name=name, email=email, phone_no=phone_no, message=message_text)
+        messages.success(request, "Your message has been sent")
+    
     return render(request, 'user_home/support_helpline_section.html')
