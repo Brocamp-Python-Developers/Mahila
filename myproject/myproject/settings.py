e82e90381b2e462df1b29278616b7027d6a2c6bb
Fauzudheen Abdul Hameed <fauzudheen1@gmail.com>
2024-03-10 18:03:15 +0530

    exposed secret key

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-2329rsj9sx3m(#n^##r@a95*v54qfbo2jb1hp1713y9l1!ffg%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
